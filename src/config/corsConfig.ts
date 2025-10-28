011be94aba29f675e1167fae72f62a6d0fb3ff8e
mary <thoeun.mary@institute.pse.ngo>
2025-10-28 07:15:52 +0700

    add style

--- src/config/corsConfig.ts
+++ src/config/corsConfig.ts
@@ -1,8 +1,7 @@
 import cors from "cors";
-
 export const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:3000"], 
-  credentials: true,               
+  origin: ["http://localhost:5173", "http://localhost:3000"],
+  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
--- src/controller/categoryController.ts
+++ src/controller/categoryController.ts
@@ -7,7 +7,7 @@ import {
   deleteCategoryService,
 } from "@/service/categoryService";
 import { CreateCategoryInput } from "@/types/category";
-
+// create category
 export const createCategory = async (req: Request, res: Response) => {
   try {
     const categoryData: CreateCategoryInput = req.body;
@@ -17,23 +17,23 @@ export const createCategory = async (req: Request, res: Response) => {
     res.status(500).json({ success: false, message: "Server error" });
   }
 };
-
+// get all categories
 export const getCategories = async (_req: Request, res: Response) => {
-  const result = await getAllCategoriesService();
-  res.status(result.success ? 200 : 400).json(result);
+  const getAllCategory = await getAllCategoriesService();
+  res.status(getAllCategory.success ? 200 : 400).json(getAllCategory);
 };
-
+// get category by id
 export const getCategoryById = async (req: Request, res: Response) => {
-  const result = await getCategoryByIdService(req.params.id);
-  res.status(result.success ? 200 : 404).json(result);
+  const getCategoryBYId = await getCategoryByIdService(req.params.id);
+  res.status(getCategoryBYId.success ? 200 : 404).json(getCategoryBYId);
 };
-
+// update category
 export const updateCategory = async (req: Request, res: Response) => {
-  const result = await updateCategoryService(req.params.id, req.body);
-  res.status(result.success ? 200 : 404).json(result);
+  const updateCategory = await updateCategoryService(req.params.id, req.body);
+  res.status(updateCategory.success ? 200 : 404).json(updateCategory);
 };
-
+// delete category
 export const deleteCategory = async (req: Request, res: Response) => {
-  const result = await deleteCategoryService(req.params.id);
-  res.status(result.success ? 200 : 404).json(result);
+  const deleteCategory = await deleteCategoryService(req.params.id);
+  res.status(deleteCategory.success ? 200 : 404).json(deleteCategory);
 };
